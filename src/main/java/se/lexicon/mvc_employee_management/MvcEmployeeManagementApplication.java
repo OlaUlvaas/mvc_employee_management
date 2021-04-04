ee6c924c046f8bb515abbc669394a6faf040e826
olaulvas <ola.ulvaas@gmail.com>
2021-04-04 13:21:26 +0200

    Package structure. Config mySql db in application.properties. Created db. Created entity.

---
+++ src/main/java/se/lexicon/mvc_employee_management/MvcEmployeeManagementApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.mvc_employee_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcEmployeeManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcEmployeeManagementApplication.class, args);
+    }
+
+}
